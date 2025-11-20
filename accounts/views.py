c25ba5c080822a32d7c9360d61e9a88dcc04607d
mario-ak37 <alfredkipkogei@proton.me>
2025-11-21 01:10:46 +0300

    add user registration to the site

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 
 # Create your views here.
 
@@ -37,3 +37,19 @@ def user_login(request):
 @login_required
 def dashboard(request):
     return render(request, "dashboard.html", {"section": "dashboard"})
+
+
+# user registration
+def register(request):
+    if request.method == "POST":
+        user_form = UserRegistrationForm(request.POST)
+
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data["password"])
+            new_user.save()
+
+            return render(request, "register_done.html", {"new_user": new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, "register.html", {"user_form": user_form})
